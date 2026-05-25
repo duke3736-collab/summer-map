1fac349ac580be6622f19fecd62723c71c71e569
park sung lae <duke3736@gmail.com>
2026-05-25 21:52:45 +0900

    chore: update adsense publisher ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <head>
         <Script 
           async 
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX" 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6635245275061755" 
           crossOrigin="anonymous" 
           strategy="afterInteractive"
         />
--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -29,7 +29,7 @@ export default function AdSense({ className = "" }: AdSenseProps) {
         style={{ display: "block", textAlign: "center" }}
         data-ad-layout="in-article"
         data-ad-format="fluid"
-        data-ad-client="ca-pub-XXXXXXXXXXXXXXXX"
+        data-ad-client="ca-pub-6635245275061755"
         data-ad-slot="XXXXXXXXXX"
       />
     </div>
